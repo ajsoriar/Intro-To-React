b78bb6aa5aba14792c4e712a70d4a7e6347d4b9d
asr <ajsoriar@gmail.com>
2017-09-13 10:27:38 +0200

    Implementing Time Travel

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -252,6 +252,7 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
@@ -270,8 +271,10 @@ class Game extends React.Component {
 
     this.doStuff();
     
-    const history = this.state.history;
+    //const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
+    
     const squares = current.squares.slice();
 
     this.logHistory( history );
@@ -279,20 +282,43 @@ class Game extends React.Component {
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
+
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+
+    console.log("jumpTo step:", step);
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     });
   }
 
   render() {
 
     const history = this.state.history;
-    const current = history[history.length - 1];
+    //const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Move #' + move :
+        'Game start';
+      return (
+        <li key={move}>
+          <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
+        </li>
+      );
+    });
 
     let status;
     if (winner) {
@@ -311,7 +337,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
